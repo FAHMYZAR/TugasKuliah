72ffa975bb3cc30807d30d8640b5d2856054be48
FAHMYZAR <dafatrans2016@gmail.com>
2026-05-20 19:45:12 +0700

    update db

--- PraktikumMYSQL/config/database.php
+++ PraktikumMYSQL/config/database.php
@@ -1,11 +1,17 @@
 <?php
 
 // $server = "mysql_db";
-$server = "localhost";
-$user = "root";
-$password = "";
-$nama_database = "perpustakaan";
+// $server = "localhost";
+// $user = "root";
+// $password = "";
+// $nama_database = "perpustakaan";
 
+// $port = 3306;
+
+$server = "mysql";
+$user = "perpus_user";
+$password = "perpus123";
+$nama_database = "perpustakaan";
 $port = 3306;
 
 $db = mysqli_connect($server, $user, $password, $nama_database, $port);
